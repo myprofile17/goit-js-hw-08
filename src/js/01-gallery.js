b096b3c4bd5603cdf65020e816e0431c1dcd27a2
Ed <mazacan@gmail.com>
2023-03-01 16:24:28 +0200

    initial commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
+import SimpleLightbox from 'Simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const itemsContainer = document.querySelector('.gallery');
+
+const galleryNewItems = createGalleryItems(galleryItems);
+
+itemsContainer.insertAdjacentHTML('afterbegin', galleryNewItems);
+
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>
+`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
 console.log(galleryItems);
